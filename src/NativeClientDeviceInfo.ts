fc05862f2adb3f035b1a344b37e5d68c8269c209
youthfulhps <ybh942002@gmail.com>
2025-02-23 21:39:56 +0900

    feat: apply get application name function to example project

--- src/NativeClientDeviceInfo.ts
+++ src/NativeClientDeviceInfo.ts
@@ -3,6 +3,7 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   getDeviceName(): string;
+  getApplicationName(): string;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ClientDeviceInfo');
--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import ClientDeviceInfo from './NativeClientDeviceInfo';
 export function getDeviceName(): string {
   return ClientDeviceInfo.getDeviceName();
 }
+
+export function getApplicationName(): string {
+  return ClientDeviceInfo.getApplicationName();
+}
